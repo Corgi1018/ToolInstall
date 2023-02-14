cd5b94b7e0c04ab33a258a383432726cb72e5f04
Corgi1018 <yujielee1018@163.com>
2023-02-14 11:54:16 +0800

    添加复制、导出权重工具，眼睛工具按钮

--- src/maya_plugin.h
+++ src/maya_plugin.h
@@ -33,8 +33,19 @@ class export_weightTool:public mayaPlugin{
   void install() override;
 };
 
+class copy_weightTool:public mayaPlugin{
+  public:
+  void install() override;
+};
+
+class eyeTool:public mayaPlugin{
+  public:
+  void install() override;
+};
+
 bool install_plugin(std::shared_ptr<mayaPlugin>& l_p);//安装插件
 std::filesystem::path get_env_path(const KNOWNFOLDERID& in_env);//获取环境变量
 void copy_file(const std::filesystem::path& in_from, const std::filesystem::path& in_to);//拷贝文件
 std::vector<std::string> setup_exe(const std::string_view& in_arg);//启动exe子进程
+
 }  // namespace maya
